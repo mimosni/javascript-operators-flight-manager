f5433f7d40bd291051d97bc3458bac4932e71e84
Mohamed Mechergui <mmechergui@cegid.com>
2021-10-05 20:56:28 +0200

    Step3

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,56 @@
 function Prices() {
+  const calculateFinalPrice = (
+    basePrice,
+    passengerVariation,
+    flightVariation
+  ) => {
+    const pricePassenger =
+      parseFloat(basePrice) + (passengerVariation / 100) * basePrice;
+    return (
+      Math.round(
+        (pricePassenger + (flightVariation / 100) * pricePassenger) * 100
+      ) / 100
+    );
+  };
 
+  const calculateDefaultFinalPrice = (basePrice, passengerType, flightType) => {
+    let passengerVar = 0;
+    switch (passengerType.toUpperCase()) {
+      case "REGULAR":
+        passengerVar = -5;
+        break;
+      case "VIP":
+        passengerVar = 5;
+        break;
+      default:
+        passengerVar = 0;
+    }
+    let flightVar = 0;
+    switch (flightType.toUpperCase()) {
+      case "ECONOMY":
+        flightVar = -3;
+        break;
+      case "BUSINESS":
+        flightVar = 10;
+        break;
+      default:
+        flightVar = 0;
+    }
+    return calculateFinalPrice(basePrice, passengerVar, flightVar);
+  };
+
+  const calculateTotalFinalPrice = (
+    seats,
+    passengerType,
+    flightType,
+    basePrice
+  ) => calculateDefaultFinalPrice(basePrice, passengerType, flightType) * seats;
+
+  return {
+    calculateFinalPrice,
+    calculateDefaultFinalPrice,
+    calculateTotalFinalPrice,
+  };
 }
 
+module.exports = Prices();
--- logic/util.js
+++ logic/util.js
@@ -4,9 +4,28 @@ function Util() {
 
   const calculateTotalNumberOfPassengers = (passengers) =>
     passengers.reduce((acc, res) => acc + res, 0);
+  const checkInput = (input) => {
+    if (!input || isNaN(input)) {
+      throw new Error("Fail input");
+    }
+  };
+  const calculateTotalDistance = (distances) =>
+    Object.values(distances).reduce((acc, res) => acc + (res > 0 ? res : 0), 0);
+  const calculateBonusPoints = (
+    businessDistances,
+    economyDistances,
+    businessBonus,
+    economyBonus
+  ) =>
+    calculateTotalDistance(businessDistances) * (businessBonus / 100) +
+    calculateTotalDistance(economyDistances) * (economyBonus / 100);
+
   return {
     calculateTotalDistributedPassengers,
     calculateTotalNumberOfPassengers,
+    checkInput,
+    calculateTotalDistance,
+    calculateBonusPoints,
   };
 }
 
